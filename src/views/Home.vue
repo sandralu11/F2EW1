430149974b4f4ec2df605b3ada50b57567958153
Sandra lu <a0960529965@gmail.com>
2021-11-10 20:30:39 +0800

    活動類別

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,9 @@
         <p>台灣的各個美景，都美不勝收。</p>
         <p> 等你一同來發現這座寶島的奧妙！</p>
         <CardsWrapper v-for="(item, i) in 8" :key="i"/>
+        <h2>活動類別</h2>
+        <p>各種不同的活動內容</p>
+        <p>邀請您一銅來共襄盛舉！</p>
     </div>
     
   </div>
@@ -27,18 +30,20 @@ export default {
 <style lang="scss" scoped>
 @import '../assets/media.scss';
 .container{
-  margin: 100px 95px;
+  margin:0 90px;
   @include mobile{
-    margin: 30px 20px;
+    margin: 0 20px;
   }
   h2{ 
     font-weight: bold;
     font-size: 30px;
+    padding-top:100px;
     color: #08A6BB;
   }
   p{
     font-size: 16px;
     color: #AEAEAE;
+    
   }
 }
 
